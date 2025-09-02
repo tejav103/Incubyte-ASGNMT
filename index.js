f78e44e5f610ef0932686632786ded7fbd8aa300
tejav103 <teja.vutukuri103@gmail.com>
2025-09-02 14:57:10 +0000

    wip

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const add = () => {
-
+const add = (str) => {
+    if(typeof str !== 'string'){
+        throw new Error('Invalid input!')
+    }
+    let delimetter = ','
+    let sum = 0
+    if(str.charAt(0)==='/' && str.charAt(1)==='/'){
+        delimetter = str.charAt(2)
+        str = str.slice(3)
+    }
+    console.log(delimetter)
+    str=str.replace('\n', delimetter)
+    console.log(str)
+    const numbers = str.split(delimetter).map(char => Number(char))
+    console.log(numbers)
+    const negativeNumebrs = numbers.filter(number => number < 0)
+    if(negativeNumebrs.length){
+        throw new Error('negative numbers not allowed '+negativeNumebrs.join(','))
+    }
+    sum = numbers.reduce((acc,a)=> acc+a, 0)
+    return sum;
 }
-modules.exports = {add}
+console.log(add(process.argv[2]))
+module.exports = {add}
--- index.test.js
+++ index.test.js
@@ -1,18 +1,19 @@
+import Mocha from "mocha";
 import { expect } from "chai";
-import {add} from './index';
+import {add} from './index.js';
 
 describe('add', () => {
     it('should throw error for non string input', () => {
-        expect(add(1,2)).throws('Invalid input')
+        expect(add(1,2)).to.throw('Invalid input!')
     })
     it('should add two numbers in the string', () => {
-        expect(add('1,2')).to.be(3)
+        expect(add('1,2')).toBe(3)
     })
     it('should handle new lines in between', () => {
-        expect(add('1\n2,3')).to.be(3)
+        expect(add('1\n2,3')).toBe(6)
     })
     it('should accept delimetter', () => {
-        expect(add('//;\n1;2')).to.be(3)
+        expect(add('//;\n1;2')).toBe(3)
     })
     it('should throw error for negative number in string', () => {
         expect(add('1,-1,-2')).throws('negative numbers not allowed -1,-2')
